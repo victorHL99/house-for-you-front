43f1c3af887a35640d75cabd0a336e17d5eefd60
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-29 14:28:23 -0300

    :lipstick: feat: added theme page for application

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  fonts: {
+    mainFont: "'Lato', sans-serif",
+  },
+  colors: {},
+  spacing: {
+    maxBodyWidth: '1024px',
+  },
+  breakpoints: {
+    mobile: '320px',
+    tablet: '481px',
+    laptop: '769px',
+    largeScreen: '1024px',
+    desktop: '1279px',
+  },
+};
+
+export default theme;
