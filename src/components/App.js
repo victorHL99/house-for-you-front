47591d3512ae37757bb3ba3bac23ab8ee6f26267
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-29 14:19:16 -0300

    :sparkles: feat: project react its running

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Projeto React</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// eslint-disable-next-line import/extensions
+import App from './components/App.js';
 
-ReactDOM.render(<App />, document.querySelector('.root'));
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(<App />, document.getElementById('root'));
