d80c65319179fbc70e7edcbc64b9c53663323545
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-03 17:41:18 -0300

    :sparkles: feat: added Home for Routes index

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Signup from '../pages/Signup';
 import Login from '../pages/Login';
+import Home from "../pages/Home"
 
 export default function Router() {
   return (
@@ -13,6 +14,7 @@ export default function Router() {
 
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
