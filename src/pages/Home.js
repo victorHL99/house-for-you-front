985e4e8c54955d4a0f586493fa4c7fdc9cea4684
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-03 17:40:26 -0300

    :sparkles: feat: create Homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+import Header from '../components/Header/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+    </div>
+  );
+}
