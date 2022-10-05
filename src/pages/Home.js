b0b0278ab0c91d16a8f480188178bbab5e9acc18
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-05 15:09:55 -0300

    :sparkles: feat: function for return all announcements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import axios from 'axios';
 // import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header/Header';
 import * as S from '../styles/style';
 
 export default function Home() {
+  const token = localStorage.getItem('token');
+
+  async function getAnnouncements() {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await axios.get(
+      `http://localhost:9000/home/announcements`,
+      config,
+    );
+    console.log(response);
+  }
   return (
     <S.PageContainer>
       <Header />
       <h1>Home</h1>
+      <button type="button" onClick={getAnnouncements}>
+        Get announcements
+      </button>
     </S.PageContainer>
   );
 }
