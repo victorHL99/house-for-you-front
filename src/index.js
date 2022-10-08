a141203ee01344cc827992f9666d81cba3b8ff12
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-07 21:17:03 -0300

    :bulb: docs: remove coments for index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line import/extensions
-import App from './components/App.js';
+import App from './components/App';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
