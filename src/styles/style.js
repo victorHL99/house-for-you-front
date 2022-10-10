baf6f1a19a6b99ed082efb74ee2f06e987af9866
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-10 15:11:05 -0300

    :lipstick: refactor: added CSS at the all styles

--- src/styles/style.js
+++ src/styles/style.js
@@ -7,6 +7,7 @@ export const AuthContainer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 10px;
+  padding: 70px;
 `;
 
 export const SignForm = styled.form`
@@ -19,15 +20,42 @@ export const SignForm = styled.form`
 
 export const PageContainer = styled.main`
   width: 100%;
+  position: absolute;
   height: calc(100vh - ${({ theme }) => theme.spacing.headerHeight});
   margin-top: ${({ theme }) => theme.spacing.headerHeight};
-  background-color: red;
+  background-color: ${({ theme }) => theme.colors.tertiary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: scroll;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.laptop}) {
-    background-color: blue;
+    padding: 0 25px;
   }
 
   @media (min-width: ${({ theme }) => theme.breakpoints.largeScreen}) {
-    background-color: yellow;
+    margin-top: ${({ theme }) => theme.spacing.headerHeight};
+  }
+`;
+
+export const PageLabel = styled.h1`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: 28px;
+  font-weight: 700;
+  padding: 30px;
+  word-break: break-all;
+  transition: all 300ms;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.laptop}) {
+    font-size: 43px;
+    padding: 30px 0;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.largeScreen}) {
+    flex-direction: column;
+    align-items: flex-start;
   }
 `;
