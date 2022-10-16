8245e56c25aaf8ec614e42722391b47a10ab850a
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-16 10:53:35 -0300

    test: create test for login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="cypress"/>
+
+import { faker } from '@faker-js/faker';
+
+describe('Login Suite Test', () => {
+  it('should login a user', () => {
+    const user = {
+      name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      profile_image: faker.image.avatar(),
+    };
+
+    cy.visit('http://localhost:3000/signup');
+    cy.get('[data-cy=name]').type(user.name);
+    cy.get('[data-cy=last_name]').type(user.last_name);
+    cy.get('[data-cy=email]').type(user.email);
+    cy.get('[data-cy=password]').type(user.password);
+    cy.get('[data-cy=profile_image]').type(user.profile_image);
+
+    cy.intercept('POST', '/signup').as('signup');
+    cy.contains('Sign Up').click();
+    cy.wait('@signup').then(interception => {
+      expect(interception.response.statusCode).to.equal(201);
+    });
+
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-cy=email]').type(user.email);
+    cy.get('[data-cy=password]').type(user.password);
+
+    cy.intercept('POST', '/login').as('login');
+    cy.contains('Login').click();
+    cy.wait('@login').then(interception => {
+      expect(interception.response.statusCode).to.equal(200);
+    });
+
+    cy.url().should('equal', 'http://localhost:3000/home');
+    cy.contains(`${user.name}`);
+  });
+});
