0b0e101be884e65ff745ccbcb143c7ea20809fe8
victorHL99 <victor.lessa@estudante.ufla.br>
2022-11-14 18:12:50 -0300

    feat: :lipstick: added images for assets

---
+++ src/assets/CodeImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CodeImage() {
+  return (
+    <div>
+      Illustration by{' '}
+      <a href="https://icons8.com/illustrations/author/mNCLibjicqSz">
+        Julia Kuptsova
+      </a>{' '}
+      from <a href="https://icons8.com/illustrations">Ouch!</a>
+    </div>
+  );
+}
