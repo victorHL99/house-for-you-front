84b1583b6a126b9171f1fd4c30eced3921dfc83e
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-27 17:36:43 -0300

    feat: :sparkles: create function for signup

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,3 +4,8 @@ export async function signIn(email, password) {
   const response = await api.post('login', { email, password });
   return response;
 }
+
+export async function signUp(userSignup) {
+  const response = await api.post('signup', userSignup);
+  return response;
+}
